4e4dbfb88dab33da4561694acf2246078904ea11
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-11-10 16:06:23 -0500

    Parse version number

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,8 @@ use std::io::prelude::*;
 use std::io::{self, BufRead, BufReader, Read};
 
 const TEST_CLASS_FILE_PATH: &str = "test.class";
-const magic: u32 = 0xCAFEBABE;
+const CLASS_FILE_HEADER: [u8; 4] = [0xCA, 0xFE, 0xBA, 0xBE];
+
 
 enum PoolKind {
     Class = 7,
@@ -60,9 +61,48 @@ struct ConstantPoolInfo {
     bytes: Vec<u8>,
 }
 
+pub enum MajorVersion {
+    JavaSE14 = 58,
+    JavaSE13 = 57,
+    JavaSE12 = 56,
+    JavaSE11 = 55,
+    JavaSE10 = 54,
+    JavaSE9 = 53,
+    JavaSE8 = 52,
+    JavaSE7 = 51,
+    JavaSE6 = 50,
+    JavaSE5 = 49,
+    JDK1_4 = 48,
+    JDK1_3 = 47,
+    JDK1_2 = 46,
+    JDK1_1 = 45,
+}
+
+impl MajorVersion {
+    pub fn from_u16(n: u16) -> MajorVersion {
+        match n {
+            58 => MajorVersion::JavaSE14,
+            57 => MajorVersion::JavaSE13,
+            56 => MajorVersion::JavaSE12,
+            55 => MajorVersion::JavaSE11,
+            54 => MajorVersion::JavaSE10,
+            53 => MajorVersion::JavaSE9,
+            52 => MajorVersion::JavaSE8,
+            51 => MajorVersion::JavaSE7,
+            50 => MajorVersion::JavaSE6,
+            49 => MajorVersion::JavaSE5,
+            48 => MajorVersion::JDK1_4,
+            47 => MajorVersion::JDK1_3,
+            46 => MajorVersion::JDK1_2,
+            45 => MajorVersion::JDK1_1,
+            _ => unimplemented!(),
+        }
+    }
+}
+
+
 struct ClassFile {
-    minor_version: u16,
-    major_version: u16,
+    version: (MajorVersion, u16),
     constant_pool_count: u16,
     constant_pool: Vec<ConstantPoolInfo>,
     access_flags: u16,
@@ -129,11 +169,10 @@ macro_rules! read_u32 {
 
 fn main() -> io::Result<()> {
     let mut reader = BufReader::new(File::open(TEST_CLASS_FILE_PATH)?);
-    let magic_number = read_u32!(reader);
-    println!("{}", magic_number.to_string());
-    assert_eq!(magic_number, 0xCAFEBABE);
+    assert_eq!(read_bytes_to_buffer!(reader, 4), CLASS_FILE_HEADER);
 
-    dbg!(magic_number);
+    let minor_version = read_u16!(reader);
+    let major_version = MajorVersion::from_u16(read_u16!(reader));
 
     Ok(())
 }
