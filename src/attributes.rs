68b04f1297157097fcc93f268b3864d09fa1ff9c
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-11-23 13:13:47 -0500

    Fix enum variant casing and consolidate attributes as struct

--- src/attributes.rs
+++ src/attributes.rs
@@ -5,82 +5,59 @@ pub struct AttributeInfo {
 }
 
 #[derive(Debug)]
-enum Attribute {
+pub struct Attribute {
+    name_index: u16,
+    attribute_type: AttributeType,
+}
+
+#[derive(Debug)]
+enum AttributeType {
     ConstantValue {
-        name_index: u16,
         const_value_index: u16,
     },
     Code {
-        name_index: u16,
         code: CodeAttribute,
     },
     StackMapTable {
-        name_index: u16,
         entries: Vec<FrameType>,
     },
     Exceptions {
-        name_index: u16,
         exception_index_table: Vec<u16>,
     },
     InnerClasses {
-        name_index: u16,
         classes: Vec<ClassInfo>,
     },
     EnclosingMethod {
-        name_index: u16,
         class_index: u16,
         method_index: u16,
     },
-    Synthetic {
-        name_index: u16,
-    },
+    Synthetic,
     Signature {
-        name_index: u16,
         signature_index: u16,
     },
     SourceFile {
-        name_index: u16,
         sourcefile_index: u16,
     },
     SourceDebugExtension {
-        name_index: u16,
         debug_extensions: Vec<u8>,
     },
     LineNumberTable {
-        name_index: u16,
         line_number_table: Vec<LineNumberTableEntry>,
     },
     LocalVariableTable {
-        name_index: u16,
         local_variable_table: Vec<LocalVariableTableEntry>,
     },
     LocalVariableTypeTable {
-        name_index: u16,
         local_variable_type_table: Vec<LocalVariableTypeTableEntry>,
     },
-    Deprecated {
-        name_index: u16,
-    },
-    RuntimeVisibleAnnotations {
-        name_index: u16,
-    },
-    RuntimeInvisibleAnnotations {
-        name_index: u16,
-    },
-    RuntimeVisibleParameterAnnotations {
-        name_index: u16,
-    },
-    RuntimeInvisibleParameterAnnotations {
-        name_index: u16,
-    },
-    AnnotationDefault {
-        name_index: u16,
-    },
-    BootstrapMethods {
-        name_index: u16,
-    },
+    Deprecated,
+    RuntimeVisibleAnnotations,
+    RuntimeInvisibleAnnotations,
+    RuntimeVisibleParameterAnnotations,
+    RuntimeInvisibleParameterAnnotations,
+    AnnotationDefault,
+    BootstrapMethods,
     Other {
-        name_index: u16,
         info: Vec<u8>,
     },
 }
@@ -117,25 +94,25 @@ enum VerificationTypeInfo {
 
 #[derive(Debug)]
 enum FrameType {
-    same_frame {},
-    same_locals_1_stack_item {
+    SameFrame,
+    SameLocals1StackItem {
         stack: [VerificationTypeInfo; 1],
     }, // Array of VerificationTypes may be incorrect
-    same_locals_1_stack_item_frame_extended {
+    SameLocals1StackItemFrameExtended {
         offset_delta: u16,
         stack: [VerificationTypeInfo; 1],
     },
-    chop_frame {
+    ChopFrame {
         offset_delta: u16,
     },
-    same_frame_extended {
+    SameFrameExtended {
         offset_delta: u16,
     },
-    append_frame {
+    AppendFrame {
         offset_delta: u16,
         locals: Vec<VerificationTypeInfo>,
     },
-    full_frame {
+    FullFrame {
         offset_delta: u16,
         locals: Vec<VerificationTypeInfo>,
         stack: Vec<VerificationTypeInfo>,
