4e4fadd3e1b5041270a0dd471c2ffc9550c7b7b7
ConnorSkees <39542938+ConnorSkees@users.noreply.github.com>
2019-11-30 16:35:12 -0500

    (temporarily) Commit test java file

---
+++ test.java
@@ -0,0 +1,11 @@
+public class test {
+    public static void main(String[] args) {
+        int x = 30;
+        int y = 3 + x + 1;
+        // String z = "hi";
+    }
+
+    public static String hi(int a, double b, String[] c) {
+        return "hi";
+    }
+}
