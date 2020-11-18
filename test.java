24b68d95c81e0f712fe73b231933b1a405ef9fab
Connor Skees <connor1skees@gmail.com>
2020-11-18 04:27:39 -0500

    ignore java files

--- test.java
+++ test.java
@@ -1,11 +1,18 @@
+import java.lang.invoke.StringConcatFactory;
+
 public class test {
     public static void main(String[] args) {
-        int x = 30;
-        int y = 3 + x + 1;
-        // String z = "hi";
+        // test t = new test();
+
+        // test.hi(t);
+        return;
     }
 
-    public static String hi(int a, double b, String[] c) {
-        return "hi";
+    // public static String hi(test t) {
+    //     return test.hi(t);
+    // }
+
+    public String hey(String x, String y, String z) {
+        return x + y + z;
     }
 }
